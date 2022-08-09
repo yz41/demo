656e7ce713bbe90eff4f11bab19c5c7f7be453c0
AFuy <244807029@qq.com>
2022-08-09 22:08:19 +0800

    1. 配置Mybatis 2. 三层的结构搭好了

---
+++ src/main/java/com/example/demo/pojo/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer uCode;
+    private String uName;
+    private String uPwd;
+    private String uType;
+    private String uMemo;
+}
---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.pojo.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper mapper;
+
+    @Override
+    public List<User> findAll() {
+        return mapper.findAll();
+    }
+}
